62edefaee87eca25ae90aacfa1d18be934a464c0
Mazin Ahmed <ahmedmaazin@gmail.com>
2021-02-22 16:01:02 +0800

    fix: example script fix

--- example/example.php
+++ example/example.php
@@ -1,12 +1,8 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use Src\NumberToThaana;
 
-$input = 11;
-$numberToThaana = new NumberToThaana($input);
-$output = $numberToThaana->convert();
-
-print_r("Input: " . $input . "\n");
-print_r("Output: " . $output . "\n");
+$numberToThaana = (new NumberToThaana("11"))->convert();
+var_dump($numberToThaana);
